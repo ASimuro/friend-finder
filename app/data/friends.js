a291ef347755b3977657e3ddccc6a28cfa604c42
Alex Simuro <aasimuro12@gmail.com>
2019-10-29 16:08:35 -0400

    friends file, survey html and javascript

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+	
+	{
+		name: "Michael Scott",
+		photo: "https://images.complex.com/complex/image/upload/c_limit,dpr_auto,q_90,w_720/fl_lossy,pg_1/xafyygcsnrjvo8uzzyii.jpg",
+		scores: [
+			"5",
+			"1",
+			"4",
+			"3",
+			"5",
+			"5",
+			"2",
+			"1",
+			"5",
+			"1"
+		]
+	},
+	{
+		name: "Jim Halpert",
+		photo: "https://pbs.twimg.com/profile_images/3171824697/ef75d90df2e65ce326acf30262df5918_400x400.jpeg",
+		scores: [
+			"3",
+			"1",
+			"4",
+			"5",
+			"3",
+			"1",
+			"1",
+			"1",
+			"4",
+			"4"
+		]
+	},
+	{
+		name: "Dwight Schrute",
+		photo: "https://upload.wikimedia.org/wikipedia/en/thumb/c/cd/Dwight_Schrute.jpg/220px-Dwight_Schrute.jpg",
+		score: [
+			"5",
+			"5",
+			"1",
+			"3",
+			"2",
+			"1",
+			"5",
+			"5",
+			"1",
+			"1"		
+		]
+	}
+];
+
+module.exports = friends;
